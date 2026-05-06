f5c9042979fa6cc44bd481ba51312ede2b17bdac
BЯΛD0K <andysecshield@outlook.com>
2026-05-06 14:28:39 -0300

    correções simples
    colocando " " nos nomes da tabela, para evitar conflito com instrucoes do sqlite

--- database/comandos.sql
+++ database/comandos.sql
@@ -1,10 +1,11 @@
 -- database: ./db.sqlite
 
 CREATE TABLE
-    links (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    titulo TEXT,
-    url TEXT,
+    "links" (
+    "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+    "titulo" TEXT,
+    "url" TEXT,
     criado_em DATETIME
-    );
+    ) STRICT;
 
+DROP TABLE "links";
