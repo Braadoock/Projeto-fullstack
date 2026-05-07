22760c9dd8601544b9e75d81e801a908bb0bc006
BЯΛD0K <andysecshield@outlook.com>
2026-05-07 17:50:07 -0300

    Novos estilos
    após terminar o desafio básico, acabei me empolgando, estilizando brincando com o CSS e criando novas funções como por exemplo o botão que altera o light mode para o dark mode.

--- front-end/app.js
+++ front-end/app.js
@@ -1,3 +1,12 @@
+
+
+
+let linkEditandoId = null
+
+
+
+
+
 function buscarlinks() {
     fetch('http://localhost:3000/links')
         .then(resposta => resposta.json())
@@ -6,11 +15,11 @@ function buscarlinks() {
             div.innerHTML = ''
             links.forEach(link => {
                 div.innerHTML += `
-                    <div>
-                        <h2>${link.titulo}<h2>
-                        <p>${link.url}<p>
-                         <button onclick="excluirLink(${link.id})">Excluir</button>
-                         <button onclick="editarLink(${link.id})">Editar</button>
+                    <div class="card">
+                        <h2>${link.titulo}</h2>
+                        <p>${link.url}</p>
+                         <button class="btn-excluir" onclick="excluirLink(${link.id})">Excluir</button>
+                         <button class="btn-editar" onclick="editarLink(${link.id}, '${link.titulo}', '${link.url}')">Editar</button>
                     </div>    
                 `
             })
@@ -28,12 +37,39 @@ function excluirLink(id) {
 function salvarLink() {
     const titulo = document.getElementById('titulo').value
     const url = document.getElementById('url').value 
+
+    if (linkEditandoId) {
+        fetch('http://localhost:3000/links/' + linkEditandoId, {
+            method: 'PUT',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({ titulo, url })
+        })
+        .then(() => {
+            linkEditandoId = null
+            buscarlinks()
+        })
+
+    } else {
+
     fetch('http://localhost:3000/links/', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json'},
         body: JSON.stringify({ titulo, url })
     })
     .then(() => buscarlinks())
+    }
+}
+
+
+function editarLink(id, titulo, url) {
+    linkEditandoId = id
+    document.getElementById('titulo').value = titulo
+    document.getElementById('url').value = url
+
+}
+
+function toggleTema() {
+    document.body.classList.toggle('light-mode')
 }
 
 
